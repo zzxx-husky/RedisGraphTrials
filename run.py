739f67e15c70aceba742d40bc5be8d13c41b7668
legend <kygx.legend@gmail.com>
2020-04-22 14:17:29 +0800

    Modify run.py

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ import redis
 from redisgraph import Graph
 import timeit
 
-r = redis.Redis(host='locahost', port=6379)
-redis_graph = Graph("graph", r)
+r = redis.Redis(host='localhost', port=6379)
+redis_graph = Graph("pm_test", r)
 
-query = ""
+qf = open('1000_edges.cypher', 'r')
+query = ''.join(qf.readlines()[1:])
 
 start = timeit.default_timer()
 redis_graph.query(query)
